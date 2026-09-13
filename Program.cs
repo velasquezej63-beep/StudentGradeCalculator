99f7859c081523f416e01667d5617de7f7daf0cd
velasquezej63-beep <velasquezej63@gmail.com>
2026-09-13 10:43:46 +0800

    Add files via upload
    A simple C# console application that calculates a student average grade and determines whether the student pass or failed.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name;
+        double grade1, grade2, grade3;
+        double average;
+
+        Console.Write("Enter student name: ");
+        name = Console.ReadLine();
+
+        Console.Write("Enter Grade 1: ");
+        grade1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Grade 2: ");
+        grade2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Grade 3: ");
+        grade3 = Convert.ToDouble(Console.ReadLine());
+
+        average = (grade1 + grade2 + grade3) / 3;
+
+        Console.WriteLine();
+        Console.WriteLine("===== STUDENT RESULT =====");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Average: " + average.ToString("F2"));
+
+        if (average >= 75)
+        {
+            Console.WriteLine("Status: PASSED");
+        }
+        else
+        {
+            Console.WriteLine("Status: FAILED");
+        }
+    }
+}
+
